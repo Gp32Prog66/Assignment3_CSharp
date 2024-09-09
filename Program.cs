ddb557fd6c5f4953785bc5a033c7990ce4780b90
Gp32Prog66 <szojo7@hotmail.com>
2024-09-09 19:56:00 -0400

    Formatting Lines of Code and Updating Comments

--- Program.cs
+++ Program.cs
@@ -5,40 +5,42 @@ Console.WriteLine("Solving the Surface Area of a Sphere");
 //Calculate the Surface Area of a Sphere. Using 4 decimals (Will Require PI)
 const double pi = 3.1415;
 
-while(true)
+while (true)
 {
 
-//User Input
-Console.WriteLine("Enter the Radius for a Sphere");
+    //User Input
+    Console.WriteLine("Enter the Radius for a Sphere");
 
-double radius = Convert.ToDouble(Console.ReadLine());
+    double radius = Convert.ToDouble(Console.ReadLine());
 
-//Checking for Valid User Input
-if(radius < 0)
-{
-    Console.WriteLine("Invalid Choice");
-    continue;
-}
-else
-{
-    while (radius > 0)
+    //Checking for Valid User Input
+    if (radius < 0)
     {
-    Console.WriteLine("You entered " + radius);
-    break;
+        Console.WriteLine("Invalid Choice");
+        continue;
     }
-}
+    else
+    {
+        while (radius > 0)
+        {
+            Console.WriteLine("You entered " + radius);
+            break;
+        }
+    }
 
-//Calculating Surface Area
-double surfaceArea;
+    //Calculating Surface Area
+    double surfaceArea;
 
-surfaceArea= 4*pi*Math.Pow(radius,2);
+    surfaceArea = 4 * pi * Math.Pow(radius, 2);
 
-//Format to 4 decimals
-Console.WriteLine("Surface Area of the Sphere is " + "{0:F04}", surfaceArea);
+    //Format to 4 decimals
+    Console.WriteLine("Surface Area of the Sphere is " + "{0:F04}", surfaceArea);
 
-//Ask User if they want to continue
-Console.WriteLine("Would you like to continue? Type yes or no");
-if(Console.ReadLine().ToLower() != "yes")
-break;
+    //Ask User if they want to continue
+    Console.WriteLine("Would you like to continue? Type yes or no");
+
+    //User doesn't say yes, end program
+    if (Console.ReadLine().ToLower() != "yes")
+        break;
 
 }
