11c24b4045983e3be700266daa4053a8bc94b5f5
Gp32Prog66 <szojo7@hotmail.com>
2024-09-09 12:34:04 -0400

    Program Repeats and made PI have 4 digits

--- Program.cs
+++ Program.cs
@@ -2,28 +2,26 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Solving the Surface Area of a Sphere");
 
-//Calculate the Surface Area of a Sphere. (Will Require PI)
-const double pi = 3.14;
+//Calculate the Surface Area of a Sphere. Using 4 decimals (Will Require PI)
+const double pi = 3.1415;
+
+while(true)
+{
 
 //User Input
 Console.WriteLine("Enter the Radius for a Sphere");
 
 double radius = Convert.ToDouble(Console.ReadLine());
 
-do
-{
-
 //Checking for Valid User Input
 if(radius < 0)
 {
     Console.WriteLine("Invalid Choice");
-    continue;
 }
 else
 {
     Console.WriteLine("You entered " + radius);
 }
-}while(radius < 0);
 
 //Calculating Surface Area
 double surfaceArea;
@@ -33,5 +31,8 @@ surfaceArea= 4*pi*Math.Pow(radius,2);
 //Format to 4 decimals
 Console.WriteLine("Surface Area of the Sphere is " + "{0:F04}", surfaceArea);
 
+Console.WriteLine("Would you like to continue? Type yes or no");
+if(Console.ReadLine().ToLower() != "yes")
+break;
 
-
+}
